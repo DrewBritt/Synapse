23818e6f1a7ef8323496bd5327d3719d4c27a861
Lyon Jenkins <lyonjenkins@gmail.com>
2019-09-24 11:52:27 -0500

    Basic Model files for Assignment, AssignmentCategory, and Grade

---
+++ src/AGGS.Data/Models/AssignmentCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AGGS.Data.Models
+{
+    public partial class AssignmentCategory
+    {
+        public int CategoryId { get; set; }
+        public int TeacherId { get; set; }
+        public string CategoryName { get; set; }
+        public int CategoryWeight { get; set; }
+    }
+}
