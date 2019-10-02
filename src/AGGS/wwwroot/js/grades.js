5b76876188b976ecd0bada8fd4961e96a8281d4e
Lyon Jenkins <lyonjenkins@gmail.com>
2019-10-02 11:09:26 -0500

    disable inputs on page load

--- src/AGGS/wwwroot/js/grades.js
+++ src/AGGS/wwwroot/js/grades.js
@@ -2,4 +2,18 @@
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/gradesHub").build();
 
+function disableInputs() {
+    let inputs = document.getElementsByTagName("input");
+    for (let i = 0; i < inputs.length; i++) {
+        inputs[i].setAttribute("disabled", "true");
+    }
+}
+
+function enableInputs() {
+    let inputs = document.getElementsByTagName("input");
+    for (let i = 0; i < inputs.length; i++) {
+        inputs[i].removeAttribute("disabled");
+    }
+}
+
 //Disable updating of grades until SignalR connection is established
