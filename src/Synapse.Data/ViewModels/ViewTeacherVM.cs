946ff17643aae46bcdec77782969015e97d42277
Drew Britt <drewbritt02@gmail.com>
2019-10-17 17:37:17 -0500

    Add ViewModel for ViewTeacher page

---
+++ src/Synapse.Data/ViewModels/ViewTeacherVM.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Synapse.Data.Models;
+
+namespace Synapse.Data.ViewModels
+{
+    public class ViewTeacherVM
+    {
+        public int TeacherId { get; set; }
+        public string TeacherFirstName { get; set; }
+        public string TeacherLastName { get; set; }
+        public string TeacherFullName 
+        {
+            get
+            {
+                return TeacherFirstName + " " + TeacherLastName;
+            }
+        }
+        public string Email { get; set; }
+
+        public List<Class> Classes { get; set; }
+    }
+}
